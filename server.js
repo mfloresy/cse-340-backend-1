a7b93d6b402b659ba9a6967faa3267d2b5506d14
Manuel Flores <mfloresyanez@byupathway.edu>
2025-05-24 19:25:25 -0400

    added error handling

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
+const utilities = require("./utilities/index")
 
 /* ***********************
  * View Engine and Templates
@@ -25,18 +26,52 @@ app.set("layout", "./layouts/layout.ejs") // not at views root
 app.use(static)
 
 // Index route
+app.get("/", utilities.handleErrors(baseController.buildHome)) 
+
 /* 
 app.get("/", function(req, res){
   res.render("index", {title: "home"})
 }) 
 */
 
-app.get("/", baseController.buildHome) 
+
+// File Not Found Route - must be last route in list
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message,
+    nav
+  })
+})
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message: err.message,
+    nav
+  })
+})
+
 
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
+
+
 const port = process.env.PORT
 const host = process.env.HOST
 
